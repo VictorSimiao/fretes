3ac00a2233e14286ec910915eb363db1e5304542
Victor Simião <victor.sreis@hotmail.com>
2021-08-16 15:51:45 -0300

    Tratando erro de Validação do cep

--- src/main/kotlin/br/com/zup/edu/FretesGrpcServer.kt
+++ src/main/kotlin/br/com/zup/edu/FretesGrpcServer.kt
@@ -1,7 +1,10 @@
 package br.com.zup.edu
 
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
+import java.lang.IllegalArgumentException
 import javax.inject.Singleton
 import kotlin.random.Random
 
@@ -12,6 +15,20 @@ class FretesGrpcServer:FretesServiceGrpc.FretesServiceImplBase(){
 
     override fun calculaFrete(request: FreteRequest?, responseObserver: StreamObserver<FreteResponse>?) {
         logger.info("Calculando frete para request: $request")
+
+        /**
+         * Cep Obrigatório
+         */
+        val cep = request?.cep
+        if(cep == null || cep.isBlank()){
+
+            val erro = Status.INVALID_ARGUMENT
+               .withDescription("cep deve ser informado")
+               .asRuntimeException()
+
+            responseObserver?.onError(erro)
+        }
+
 
         val response = FreteResponse.newBuilder()
             .setCep(request!!.cep)
